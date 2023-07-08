8a7a99f47349519f344937bec8d104ff17a98134
unknown <babatundeyusuffolorunso@gmail.com>
2023-07-08 14:20:00 +0100

    adding changes

--- src/component/pages/Home/ProductCard.js
+++ src/component/pages/Home/ProductCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import * as IMG from '../../images/images'
 
 const ProductCard = ({title,para,btnTxt,img}) => {
   return (
-    <div class="card bgCard p-2" >
-        <img src={require('../../images/Dyleum.svg').default} style={{width:'70px'}} alt=""/>
+    <div class="card bgCard pt-2 pt-md-3 px-2 px-3  ">
+        <img src={img}  style={{width:'35px'}} alt=""/>
         <div class="card-body p-0">
-            <h5 class="card-title textColor1 fs-6"> {title}</h5>
+            <h5 class="card-title textColor1 fs-6 py-2"> {title}</h5>
             <p class="card-text">{para}</p>
             <Link  class="btn btn-primary">{btnTxt}</Link>
         </div>
