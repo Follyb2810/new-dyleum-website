4095eed1be2a0f2b9bc367c2571bdd976d7a5f87
unknown <babatundeyusuffolorunso@gmail.com>
2023-07-12 10:04:08 +0100

    adding changes

--- src/component/pages/Home/ProductCard.js
+++ src/component/pages/Home/ProductCard.js
@@ -5,7 +5,7 @@ import * as IMG from '../../images/images'
 const ProductCard = ({title,para,btnTxt,img}) => {
   return (
     <div class="card bgCard p-3">
-        <img src={img}  style={{width:'35px'}} alt=""/>
+        <img src={img}  style={{width:'40px'}} alt=""/>
         <div class="card-body p-0">
             <h5 class="card-title textColor1 fs-6 mt-4"> {title}</h5>
             <p class="card-text fs-16 mt-3">{para}</p>
