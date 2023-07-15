cf73bea10d5128e7bf47b89aaf34e9a6b5d45051
unknown <babatundeyusuffolorunso@gmail.com>
2023-07-15 17:31:09 +0100

    adding changes

--- src/component/pages/Home/Rell.js
+++ src/component/pages/Home/Rell.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Rell = () => {
   return (
     <div className='container'>
-    <img src={require('../../images/Roadmaps/Roadmaptablet.png')} alt='' className='img-fluid d-none'/>
-      <img src={require('../../images/Roadmaps/Roadmapdesktop.png')} alt='' className='img-fluid  '/>
+    <img src={require('../../images/Roadmaps/Roadmaptablet.png')} alt='' className='img-fluid d-none text-center'/>
+      <img src={require('../../images/Roadmaps/Roadmapdesktop.png')} alt='' className='img-fluid text-center'/>
       <img src={require('../../images/Roadmaps/Roadmapmobile.png')} alt='' className='img-fluid d-none'/>
     </div>
   )
