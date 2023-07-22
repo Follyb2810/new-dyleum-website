6ac75f7ba5c1407f244b1ad0876e454d69d913b2
unknown <babatundeyusuffolorunso@gmail.com>
2023-07-22 09:59:25 +0100

    adding changes

--- src/component/pages/Home/Hero.js
+++ src/component/pages/Home/Hero.js
@@ -8,9 +8,8 @@ const Hero = () => {
           <div className='col text-center'>
             <div className='text-center'>
               <h2 className='text-center heroContent text-white pb-1'>AI powered <span className='textColor1'>decentralized</span> exchange</h2>
-              <p className='text-center py-1'>Empowering web3 with AI powered products</p>
-              <button className='btn primaryB btnPrimary mx-auto mt-1 mt-md-2'><a href='#product'>Discover product</a></button>
-              
+              <p className='text-center py-1 fs-18'>Empowering web3 with AI powered products</p>
+              <button className='btn primaryB btnPrimary mx-auto mt-1 mt-md-2'><a href='#product'>Discover product</a></button>  
               </div>
           </div>
         </div>
